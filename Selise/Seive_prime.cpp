a42e7fe209f2decc8683a90f72b01b9584535123
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-05-12 23:43:08 -0700

    Seive of eratosthenes is added.

--- Selise/Seive_prime.cpp
+++ Selise/Seive_prime.cpp
@@ -1,1 +1,55 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+#define SZ 10000
+
+int N;
+
+int prime[SZ];
+
+void seive_of_eratosthenes()
+{
+    //vector<int >prime;
+
+//    memset(prime, 1, sizeof(prime));
+
+    for(int i = 1 ;  i<= N ; i++)
+    {
+        prime[i] = 1;
+    }
+
+    for(int i = 2 ; i*i <= N; i++ )
+    {
+        if(prime[i]  == 1)
+        {
+            //printf("-------------\n");
+            for(int j = i*i ; j <= N ; j += i)
+            {
+                prime[j] = 0;
+            }
+        }
+    }
+
+
+    for(int i = 2 ; i <= N; i++)
+    {
+        if(prime[i])
+        {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+}
+
+
+
+int main()
+
+{
+    scanf("%d", &N);
+
+    seive_of_eratosthenes();
+
+    return 0;
+}
