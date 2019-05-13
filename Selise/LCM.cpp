76d257db926e6f63c09a65c35bc69cf996799602
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-05-12 23:31:00 -0700

    LCm is added.

---
+++ Selise/LCM.cpp
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+
+int N,M;
+
+int GCD(int a, int b)
+{
+    if(a == 0)
+    {
+        return b;
+    }
+
+    if(b == 0)
+    {
+        return a;
+    }
+
+    if(a == b)
+    {
+        return a;
+    }
+
+    if(a > b)
+        return GCD(a-b,b);
+
+    return GCD(a, b-a);
+}
+
+
+int main()
+{
+    scanf("%d %d", &N, &M);
+
+    int g = GCD(N,M);
+
+    g = (N*M)/g;
+
+    printf("%d\n",g);
+
+    return 0;
+}
